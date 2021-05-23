10fbe38ed3d663cd4e8c4a08f35f0ac3d8be5113
Misha Danilov <62474815+MishaDanilov@users.noreply.github.com>
2021-05-23 23:26:15 +0300

    refactor: refactor code

--- db.js
+++ db.js
@@ -1,16 +1,20 @@
 const Sequelize = require('sequelize');
-                                //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
-    dialect: 'postgres'
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, { //параметры беруться из файла .env
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: "5433",
+    operatorsAliases: false,
+    define: {timestamps: false}
 })
 
-sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
+sequelize.authenticate()
+.then(()=>console.log("Connected to DB"))
+.catch((err)=>console.log(`Error: ${err}`))
 
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-)
+
+module.exports = {//<db.js экспортировал только объект класса Sequelize, но не экспортировал сам класс Sequelize>.<добавил в экспорт класс Sequelize>. Исправлена строка <18> в файле <db.js>
+    db: sequelize,
+    dataTypes: Sequelize
+} //<ошибка "db.sync is not a function" в файле app.js>.<экспортировал объект sequelize>. Исправлена строка <18> в файле <db.js>
